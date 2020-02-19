c6a72c483fce98170ae3806cf3a8245da5a767c1
Eungu <kiwi9084@gmail.com>
2020-02-19 18:12:03 +0900

    Ask Permission (Before Android Q)

--- app/src/main/java/com/eungu/lineplusnote/MemoList/MainActivity.java
+++ app/src/main/java/com/eungu/lineplusnote/MemoList/MainActivity.java
@@ -1,14 +1,23 @@
 package com.eungu.lineplusnote.MemoList;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.eungu.lineplusnote.DBManager.DBData;
 import com.eungu.lineplusnote.DBManager.DBManager;
@@ -19,7 +28,10 @@ import com.eungu.lineplusnote.R;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
     public static int ADD_REQUEST_CODE = 1;
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 98;
+
     ArrayList<MemoListItem> list;
     MemoListAdapter listAdapter;
     RecyclerView recyclerView;
@@ -27,6 +39,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                AlertDialog.Builder oDialog = new AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Light_Dialog);
+                oDialog.setTitle("권한 요청")
+                        .setMessage("메모를 내부저장소에 저장/읽기 위해 접근 권한이 필요합니다.")
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                            }
+                        })
+                        .setCancelable(false)
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            }
+        }
 
         init_list();
         setList();
@@ -71,6 +103,20 @@ public class MainActivity extends AppCompatActivity {
             if(resultCode == RESULT_OK){
                 setList();
             }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "권한이 없어 앱을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
         }
     }
 }
